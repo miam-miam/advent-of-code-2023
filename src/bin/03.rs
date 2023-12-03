eae130d58edf452bed9d31df190fe4afc64482d7
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-12-03 11:31:00 +0000

    Simplify day 3

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
-
-const DIAGONALS: [(i32, i32); 8] = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (1, -1) ,(-1, 1)];
+use advent_of_code::helpers::CROSS_PLUS;
 
 #[derive(Ord, PartialOrd, Eq, PartialEq)]
 enum Character {
@@ -12,71 +11,22 @@ enum Character {
 
 fn next_to_symbol(grid: &Vec<Vec<Character>>, x: i32, y: i32) -> bool {
     if 0 <= x && x < grid.len() as i32 && 0 <= y && y < grid[0].len() as i32 {
-        grid[x as usize][y as usize] == Character::Symbol
+        grid[x as usize][y as usize] == Character::Symbol || grid[x as usize][y as usize] == Character::Gear
     } else {
         false
     }
 }
 
-fn next_to_gear(grid: &Vec<Vec<Character>>, x: i32, y: i32) -> Option<usize> {
+fn next_to_number(grid: &Vec<Vec<Character>>, x: i32, y: i32) -> Option<usize> {
     if 0 <= x && x < grid.len() as i32 && 0 <= y && y < grid[0].len() as i32 {
-        match grid[x as usize][y as usize]  {
-            Character::Number(num) => Some(num),
-            _ => None
+        if let Character::Number(num) = grid[x as usize][y as usize] {
+            return Some(num);
         }
-    } else {
-        None
     }
+    None
 }
 
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut nums = vec![];
-    let grid = input.lines().map(|l| {
-        let mut num = 0;
-        let v = l.chars().map(|c| {
-            if c.is_ascii_digit() {
-                num = num * 10 + (c as u32 - '0' as u32);
-                Character::Number(nums.len())
-            } else {
-                if num != 0 {
-                    nums.push(num);
-                    num = 0
-                }
-                if c == '.' {
-                    Character::Dot
-                } else {
-                    Character::Symbol
-                }
-            }
-        }).collect_vec();
-        if num != 0 {
-            nums.push(num);
-        }
-        v
-    }).collect_vec();
-
-    let g = &grid;
-
-    Some(grid.iter().enumerate().flat_map(|(x, l)| {
-        l.iter().enumerate().filter_map(move |(y, c)| {
-            let (x, y) = (x as i32, y as i32);
-            match c {
-                Character::Number(idx) => {
-                    if DIAGONALS.iter().any(|(dx, dy)| next_to_symbol(g, x + dx, y + dy)) {
-                        Some(idx)
-                    } else {
-                        None
-                    }
-                }
-                _ => None
-            }
-        })
-    }).unique().map(|idx| nums[*idx]).sum())
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-
+fn parse_input(input: &str) -> (Vec<u32>, Vec<Vec<Character>>) {
     let mut nums = vec![];
     let grid = input.lines().map(|l| {
         let mut num = 0;
@@ -103,17 +53,39 @@ pub fn part_two(input: &str) -> Option<u32> {
         }
         v
     }).collect_vec();
+    (nums, grid)
+}
+
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (nums, grid) = parse_input(input);
+
+    let g = &grid;
+
+    Some(grid.iter().enumerate().flat_map(|(x, l)| {
+        l.iter().enumerate().filter_map(move |(y, c)| {
+            match c {
+                Character::Number(idx) if CROSS_PLUS.iter().any(|(dx, dy)| next_to_symbol(g, x as i32 + dx, y as i32 + dy)) => {
+                    Some(idx)
+                }
+                _ => None
+            }
+        })
+    }).unique().map(|idx| nums[*idx]).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (nums, grid) = parse_input(input);
 
     let g = &grid;
     let n = &nums;
 
     Some(grid.iter().enumerate().flat_map(|(x, l)| {
         l.iter().enumerate().filter_map(move |(y, c)| {
-            let (x, y) = (x as i32, y as i32);
             match c {
                 Character::Gear => {
-                    let (num1, num2) = DIAGONALS.iter().filter_map(|(dx, dy)| {
-                        next_to_gear(g, x + dx, y + dy)
+                    let (num1, num2) = CROSS_PLUS.iter().filter_map(|(dx, dy)| {
+                        next_to_number(g, x as i32 + dx, y as i32 + dy)
                     }).unique().collect_tuple()?;
                     Some(n[num1] * n[num2])
                 }
